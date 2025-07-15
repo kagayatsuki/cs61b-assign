280e175f2349b90e9807d0ef3faf84b15ccd7112
kagayatsuki <huyihao12306@163.com>
2025-07-16 00:18:45 +0800

    updated proj1

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,49 @@
+package deque;
+
+import java.util.Comparator;
+
+
+    class MaxArrayDeque<T> extends ArrayDeque<T> {
+
+        private Comparator<T> comparator;
+        public MaxArrayDeque(Comparator<T> c) {
+            super();
+            this.comparator = c;
+        }
+        public T max(){
+            if (isEmpty()) {
+                return null;
+            }
+            T max=get(0);
+            for (int i=0;i<size();i++){
+                T element = get(i);
+                if (comparator.compare(element,max)>0){
+                    max = element;
+
+                }
+            }
+            return max;
+        }
+        public T max(Comparator<T> c){
+            if (isEmpty()) {
+                return null;
+            }
+            T max=get(0);
+            for (int i=0;i<size();i++){
+                T element = get(i);
+                if (c.compare(element,max)>0){
+                    max = element;
+                }
+            }
+            return max;
+        }
+
+
+
+
+
+    }
+
+
+
+
